efa4cb17addada34212c42a706d6b198936a587b
LilleCerebellum <Soellingvraa72@gmail.com>
2025-06-06 09:47:07 +0200

    some update shit

--- fileRead.py
+++ fileRead.py
@@ -3,5 +3,6 @@ import pdfplumber
 
     
 with pdfplumber.open("C:/Users/soell_7pbpycu/Desktop/Synopsis 3 semester projekt.pdf") as f:
-    text = pdf.extract_text()
-    print(text)
+    for i in f. pages:
+        print(i. extract_text())
+    
