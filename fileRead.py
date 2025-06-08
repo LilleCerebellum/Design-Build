429dd05beb7432c4a6368c26d37052352822a888
LilleCerebellum <Soellingvraa72@gmail.com>
2025-06-09 01:41:26 +0200

    Search for specifc keyword

--- fileRead.py
+++ fileRead.py
@@ -1,8 +1,24 @@
 import pdfplumber
+import re
 
-
+pattern = re.compile(r"børn \d+-\d+", re.IGNORECASE)
     
 with pdfplumber.open("C:/Users/soell_7pbpycu/Desktop/Synopsis 3 semester projekt.pdf") as f:
     for i in f. pages:
-        print(i. extract_text())
+        Text = i. extract_text()
+        if Text:
+            print("it extracted")
+            matches = pattern.findall(Text)
+            print(*matches, sep='\n')
     
+
+
+"""
+test_string = "Example text without the forbidden word."
+result = pattern.match(test_string)
+if result:
+    print("No forbidden word found.")
+else:
+    print("Forbidden word detected.")
+    
+"""
